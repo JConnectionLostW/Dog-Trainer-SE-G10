9664c2242eaf57f75b9edf5d42ac9877e828d645
JConnectionLostW <sharknado345@gmail.com>
2023-10-22 01:10:08 -0500

    Complete restart of programming architecture
    still trying to figure out how react works

---
+++ dog-trainer/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ dog-trainer/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const NavBar = () => (
+    <nav className = "app.navbar">
+        <ul className="app.navbar.links">
+            <li className="p.opensans"><a href = "#home">Home</a></li>
+            <li className="p.opensans"><a href = "#sign-in">Sign In/Register</a></li>
+            <li className="p.opensans"><a href = "#about">About</a></li>
+            <li className="p.opensans"><a href = "#contact">contact</a></li>
+        </ul>
+
+    </nav>
+)
+export default NavBar;
