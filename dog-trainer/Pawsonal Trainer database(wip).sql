3974d78a3bc067fc8592106de73166297c3b1042
JKGarcia0 <145131451+JKGarcia0@users.noreply.github.com>
2023-10-23 11:13:02 -0500

    Add files via upload

---
+++ dog-trainer/Pawsonal Trainer database(wip).sql
@@ -0,0 +1,58 @@
+#drop existing database
+drop database if exists PawsonalTrainer;
+
+#create and use database
+create database PawsonalTrainer;
+use PawsonalTrainer;
+
+#create all tables
+
+#table Business
+create table Business(
+BId int primary key,
+name varchar(255),
+title varchar(255),
+address varchar(255),
+year int,
+phone int,
+email varchar(225)
+);
+
+#table dev
+create table dev(
+eId int primary key,
+name varchar(255),
+address varchar(255),
+phone int,
+email varchar(255),
+salary int,
+BId int,
+foreign key(BId) references Business(BId)
+);
+
+#table Customer
+create table Customer(
+cId int primary key
+name varchar(255),
+address varchar(255),
+phone int,
+email varchar(255),
+);
+
+#table Dog
+create table Dog(
+dId int primary key,
+name varchar(255),
+breed varchar(30),
+custId int,
+foreign key(custId) references Customer(cId)
+);
+
+#table Service
+create table Service(
+sId int primary key,
+sName varchar(255),
+vidId int,
+petId int,
+foreign key(vidId) references dev(eId),
+foreign key(petId) references Dog(dId),
