f21cf69cd91dff74e62b8e4908557824130c1d19
JKGarcia0 <145131451+JKGarcia0@users.noreply.github.com>
2023-10-24 15:43:04 -0500

    Update Login.jsx

--- dog-trainer/src/Components/Login.jsx
+++ dog-trainer/src/Components/Login.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types'; //allows use of setToken prop
 
-function Login = () =>{
+//function that makes a post request to server
+
+async function loginUser(credentials) {
+  //connects to server to fetch
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+export default function Login({setToken}){
   // State to store user input
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-
-  const handleLogin = (e) => {
+//create form submit handler to do logins
+//then call it later with form onSubmit
+  const handleLogin = async e => {
     e.preventDefault();
     // You would typically perform authentication here
-    console.log('Username:', username);
-    console.log('Password:', password);
+    //console.log('Username:', username); old data
+    //console.log('Password:', password); old data
+      username,
+      password
+  });
+  setToken(token);
   };
 
   return (
@@ -41,4 +61,6 @@ function Login = () =>{
   );
 }
 
-export default Login;
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
