60a5d95226ccf620c69810ecd329c9894b7157d2
JKGarcia0 <145131451+JKGarcia0@users.noreply.github.com>
2023-11-14 20:08:01 -0600

    Update Register.jsx

--- dog-trainer/src/Components/Register.jsx
+++ dog-trainer/src/Components/Register.jsx
@@ -4,13 +4,35 @@ import Axios from 'axios';
 function Register() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [error, setError] = useState('');
 
   const handleRegistration =() => {
+    //start of changes
+    //check username and password for proper format
+    // Validate username and password format
+    const usernamePattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Requires "@" in the username
+
+    if (!usernamePattern.test(username)) {
+      setError('Invalid username. Please enter a valid email address.');
+      return;
+    }
+
+    // Validate other criteria for the password (if needed)
+    const passwordPattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+    if (!passwordPattern.test(password)) {
+      setError('Invalid password. It must contain at least 8 characters, including a number and a special character (!@#$%^&*).');
+      return;
+    }
+    //end of changes
+    // If validation passes, move to registration logic
+    // ...
+
     Axios.post("http://localhost:8080/create", {
       username: username, 
       password: password
     }).then(()=>{console.log("success");
+    // Clear any previous errors
+    setError('');
   });
   };
  /* const handleRegistration = async () => {
